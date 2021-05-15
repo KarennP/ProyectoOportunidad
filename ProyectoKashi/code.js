90e57f2b6a444d47bcf4dfa3a4233dddfe1d907e
Karen <41969662+KarennP@users.noreply.github.com>
2021-05-14 19:13:16 -0500

    first commit

---
+++ ProyectoKashi/code.js
@@ -0,0 +1,36 @@
+
+function nosotros()
+{
+      document.getElementById("nosotros").style.display = "block";
+	document.getElementById("proyectos").style.display = "none";
+	document.getElementById("donaciones").style.display = "none";
+	document.getElementById("contacto").style.display = "none";
+
+} 
+
+function proyectos()
+{
+      document.getElementById("nosotros").style.display = "none";
+	document.getElementById("proyectos").style.display = "block";
+	document.getElementById("donaciones").style.display = "none";
+	document.getElementById("contacto").style.display = "none";
+
+} 
+
+function donaciones()
+{
+      document.getElementById("nosotros").style.display = "none";
+	document.getElementById("proyectos").style.display = "none";
+	document.getElementById("donaciones").style.display = "block";
+	document.getElementById("contacto").style.display = "none";
+
+} 
+
+function contacto()
+{
+      document.getElementById("nosotros").style.display = "none";
+	document.getElementById("proyectos").style.display = "none";
+	document.getElementById("donaciones").style.display = "block";
+	document.getElementById("contacto").style.display = "none";
+
+} 
